176ece0bfc4144b1e9e54b164718737a15825ce0
Infarh <shmachilin@yandex.ru>
2020-03-30 06:51:57 +0300

    Рефакторинг, обновление пакетов

--- MathCore.EF7/Extensions/DbSetExtensions.cs
+++ MathCore.EF7/Extensions/DbSetExtensions.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
