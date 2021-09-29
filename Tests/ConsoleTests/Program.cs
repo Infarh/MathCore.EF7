b922cc4ee716664607e1ee227127d7a8555a8b3a
Infarh <shmachilin@yandex.ru>
2021-09-29 22:10:49 +0300

    Тесты

---
+++ Tests/ConsoleTests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static Func<T, T> Y<T>(Func<Func<T, T>, Func<T, T>> F) => t => F(Y(F))(t);
+
+            var files = Y<(DirectoryInfo dir, IEnumerable<DirectoryInfo> sub_dirs)>(
+                GetSubDirs => dir_info =>
+                {
+                    var dir = dir_info.dir;
+                    if (dir.FullName.Length > 70)
+                        return (dir_info.dir, dir_info.sub_dirs);
+
+                    var sub_dirs = dir.EnumerateDirectories();
+
+                    return (dir, sub_dirs);
+                });
+
+        }
+
+        
+    }
+}
---
+++ Tests/MathCore.EF7.Tests/Repositories/DbRepositoryTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathCore.EF7.Repositories;
+using MathCore.EF7.Repositories.Base;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathCore.EF7.Tests.Repositories
+{
+    [TestClass]
+    public class DbRepositoryTests
+    {
+        private class TestDbRepository : DbRepository<TestDbEntity>
+        {
+            public TestDbRepository(TestDbContext Context) : base(Context) { }
+        }
+
+        private class TestDbEntity
+            : IEntity
+        {
+            public int Id { get; set; }
+            public string Value { get; set; }
+
+            public override string ToString() => $"[id:{Id}] {Value}";
+        }
+
+        private class TestDbContext : DbContext
+        {
+            public DbSet<TestDbEntity> Values { get; set; }
+
+            public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+        }
+
+        private TestDbContext _Context;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<TestDbContext>().UseInMemoryDatabase("TestDb").Options;
+            _Context = new TestDbContext(options);
+        }
+
+        [TestMethod]
+        public async Task AddEntityTest()
+        {
+            var repository = new TestDbRepository(_Context);
+
+            var source_item1 = new TestDbEntity { Value = "Item1" };
+            var source_item2 = new TestDbEntity { Value = "Item2" };
+
+            var added_item1 = await repository.Add(source_item1);
+            var added_item2 = await repository.Add(source_item2);
+
+            var added_item1_id = added_item1.Id;
+            var added_item2_id = added_item2.Id;
+
+            var db_item1 = await repository.Get(added_item1_id);
+            var db_item2 = await repository.Get(added_item2_id);
+
+            var all_items = await repository.Items.ToArrayAsync();
+
+            Assert.That.Value(db_item1)
+               .Where(item => item.Id).CheckEquals(added_item1_id)
+               .Where(item => item.Value).CheckEquals(source_item1.Value);
+            Assert.That.Value(db_item2)
+               .Where(item => item.Id).CheckEquals(added_item2_id)
+               .Where(item => item.Value).CheckEquals(source_item2.Value);
+
+            Assert.That.Collection(all_items)
+               .Contains(item => item.Value == source_item1.Value)
+               .Contains(item => item.Value == source_item2.Value);
+
+        }
+    }
+}
