cf38bef30267eb09bb62bc9245cb11c51bf45685
APlatonenkov <platonenkov87@gmail.com>
2021-09-30 15:51:58 +0300

    адрес сервера для клиента

--- MathCore.EF7.Clients/WebRepository.cs
+++ MathCore.EF7.Clients/WebRepository.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using MathCore.EF7.Interfaces.Entities;
 using MathCore.EF7.Interfaces.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MathCore.EF7.Clients
@@ -16,21 +17,28 @@ namespace MathCore.EF7.Clients
     /// <summary> Клиент к репозиторию </summary>
     /// <typeparam name="TEntity">тип сущности</typeparam>
     /// <typeparam name="TKey">тип идентификатора</typeparam>
-    public class WebRepository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : IEntity<TKey>
+    public class WebRepository<TEntity, TKey> : BaseClient, IRepository<TEntity, TKey> where TEntity : IEntity<TKey>
     {
-        /// <summary> Клиент </summary>
-        protected readonly HttpClient _Client;
         /// <summary> логгер </summary>
         protected readonly ILogger<WebRepository<TEntity, TKey>> _Logger;
 
-        /// <summary> Конструктор </summary>
-        /// <param name="client">клиент</param>
+        /// <summary> Конструктор - адрес клиента api/TEntity</summary>
+        /// <param name="configuration">Конфигурация</param>
         /// <param name="logger">логгер</param>
-        public WebRepository(HttpClient client, ILogger<WebRepository<TEntity, TKey>> logger)
+        public WebRepository(IConfiguration configuration, ILogger<WebRepository<TEntity, TKey>> logger) : base(configuration, $"api/{nameof(TEntity)}")
         {
-            _Client = client;
             _Logger = logger;
         }
+
+        /// <summary> Конструктор </summary>
+        /// <param name="configuration">Конфигурация</param>
+        /// <param name="logger">логгер</param>
+        /// <param name="serviceAddress">адрес сервиса</param>
+        public WebRepository(IConfiguration configuration, ILogger<WebRepository<TEntity, TKey>> logger, string serviceAddress) : base(configuration, serviceAddress)
+        {
+            _Logger = logger;
+        }
+
 
         private const string BaseLogRow = "Запрос к репозиторию";
 
@@ -44,7 +52,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(IsEmpty)}");
 
-            var response = await _Client.GetAsync($"isempty", Cancel).ConfigureAwait(false);
+            var response = await _Client.GetAsync($"{ServiceAddress}/isempty", Cancel).ConfigureAwait(false);
             return response.StatusCode != HttpStatusCode.NotFound && response.IsSuccessStatusCode;
         }
 
@@ -53,7 +61,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(ExistId)} - {ToValueRow(Id)}");
 
-            var response = await _Client.GetAsync($"exist/id/{Id}", Cancel).ConfigureAwait(false);
+            var response = await _Client.GetAsync($"{ServiceAddress}/exist/id/{Id}", Cancel).ConfigureAwait(false);
             return response.StatusCode != HttpStatusCode.NotFound && response.IsSuccessStatusCode;
         }
 
@@ -62,7 +70,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(Exist)} - {ToValueRow(item.Id)}");
 
-            var response = await _Client.PostAsJsonAsync($"exist", item, Cancel).ConfigureAwait(false);
+            var response = await _Client.PostAsJsonAsync($"{ServiceAddress}/exist", item, Cancel).ConfigureAwait(false);
             return response.StatusCode != HttpStatusCode.NotFound && response.IsSuccessStatusCode;
         }
 
@@ -70,14 +78,14 @@ namespace MathCore.EF7.Clients
         public async Task<int> GetCount(CancellationToken Cancel = default)
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(GetCount)}");
-            return await _Client.GetFromJsonAsync<int>("count", Cancel).ConfigureAwait(false);
+            return await _Client.GetFromJsonAsync<int>($"{ServiceAddress}/count", Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
         public async Task<IEnumerable<TEntity>> GetAll(CancellationToken Cancel = default)
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(GetAll)}");
-            return await _Client.GetFromJsonAsync<IEnumerable<TEntity>>("", Cancel).ConfigureAwait(false);
+            return await _Client.GetFromJsonAsync<IEnumerable<TEntity>>($"{ServiceAddress}", Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
@@ -85,7 +93,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(Get)} - {ToValueRow(Skip, Count)}");
 
-            return await _Client.GetFromJsonAsync<IEnumerable<TEntity>>($"items[{Skip}:{Count}]", Cancel).ConfigureAwait(false);
+            return await _Client.GetFromJsonAsync<IEnumerable<TEntity>>($"{ServiceAddress}/items[{Skip}:{Count}]", Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
@@ -93,7 +101,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(GetPage)} - {ToValueRow(PageNumber, PageSize)}");
 
-            var response = await _Client.GetAsync($"page[{PageNumber}/{PageSize}]", Cancel).ConfigureAwait(false);
+            var response = await _Client.GetAsync($"{ServiceAddress}/page[{PageNumber}/{PageSize}]", Cancel).ConfigureAwait(false);
 
             if (response.StatusCode == HttpStatusCode.NotFound)
                 return new PageItems
@@ -130,7 +138,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(GetById)} - {ToValueRow(Id)}");
 
-            return await _Client.GetFromJsonAsync<TEntity>($"{Id}", Cancel).ConfigureAwait(false);
+            return await _Client.GetFromJsonAsync<TEntity>($"{ServiceAddress}/{Id}", Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
@@ -138,7 +146,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(Add)}");
 
-            var response = await _Client.PostAsJsonAsync("", item, Cancel).ConfigureAwait(false);
+            var response = await _Client.PostAsJsonAsync($"{ServiceAddress}", item, Cancel).ConfigureAwait(false);
             var result = await response
                .EnsureSuccessStatusCode()
                .Content
@@ -151,7 +159,7 @@ namespace MathCore.EF7.Clients
         public async Task AddRange(IEnumerable<TEntity> items, CancellationToken Cancel = default)
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(AddRange)}");
-            await _Client.PostAsJsonAsync("range", items, Cancel).ConfigureAwait(false);
+            await _Client.PostAsJsonAsync($"{ServiceAddress}/range", items, Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
@@ -159,7 +167,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(Update)} - {item.Id}");
 
-            var response = await _Client.PutAsJsonAsync("", item, Cancel).ConfigureAwait(false);
+            var response = await _Client.PutAsJsonAsync($"{ServiceAddress}", item, Cancel).ConfigureAwait(false);
             var result = await response
                .EnsureSuccessStatusCode()
                .Content
@@ -173,7 +181,7 @@ namespace MathCore.EF7.Clients
         public async Task<TEntity> UpdateById(TKey id, Action<TEntity> ItemUpdated, CancellationToken Cancel = default)
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(UpdateById)} - {ToValueRow(id)}");
-            var response = await _Client.PutAsJsonAsync($"id/{id}", ItemUpdated, Cancel).ConfigureAwait(false);
+            var response = await _Client.PutAsJsonAsync($"{ServiceAddress}/id/{id}", ItemUpdated, Cancel).ConfigureAwait(false);
 
             var result = await response
                .EnsureSuccessStatusCode()
@@ -188,7 +196,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(UpdateRange)}");
 
-            await _Client.PutAsJsonAsync($"range", items, Cancel).ConfigureAwait(false);
+            await _Client.PutAsJsonAsync($"{ServiceAddress}/range", items, Cancel).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
@@ -196,7 +204,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(Delete)} - {ToValueRow(item.Id)}");
 
-            var request = new HttpRequestMessage(HttpMethod.Delete, "")
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"{ServiceAddress}")
             {
                 Content = JsonContent.Create(item)
             };
@@ -218,7 +226,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(DeleteRange)}");
 
-            var request = new HttpRequestMessage(HttpMethod.Delete, "range")
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"{ServiceAddress}/range")
             {
                 Content = JsonContent.Create(items)
             };
@@ -231,7 +239,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(DeleteById)} - {ToValueRow(id)}");
 
-            var response = await _Client.DeleteAsync($"{id}", Cancel).ConfigureAwait(false);
+            var response = await _Client.DeleteAsync($"{ServiceAddress}/{id}", Cancel).ConfigureAwait(false);
             if (response.StatusCode == HttpStatusCode.NotFound)
                 return default;
 
@@ -249,7 +257,7 @@ namespace MathCore.EF7.Clients
         {
             _Logger.Log(LogLevel.Debug, $"{BaseLogRow} {nameof(TEntity)} {nameof(SaveChanges)}");
 
-            return await _Client.GetFromJsonAsync<int>("save", Cancel).ConfigureAwait(false);
+            return await _Client.GetFromJsonAsync<int>($"{ServiceAddress}/save", Cancel).ConfigureAwait(false);
         }
 
         #endregion
@@ -259,7 +267,12 @@ namespace MathCore.EF7.Clients
     public class WebRepository<T> : WebRepository<T, int> where T : IEntity<int>
     {
         /// <inheritdoc />
-        public WebRepository(HttpClient client, ILogger<WebRepository<T, int>> logger) : base(client, logger)
+        public WebRepository(IConfiguration configuration, ILogger<WebRepository<T, int>> logger) : base(configuration, logger)
+        {
+        }
+
+        /// <inheritdoc />
+        public WebRepository(IConfiguration configuration, ILogger<WebRepository<T, int>> logger, string serviceAddress) : base(configuration, logger, serviceAddress)
         {
         }
     }
