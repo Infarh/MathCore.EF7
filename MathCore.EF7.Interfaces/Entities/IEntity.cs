94d8c1d86d2366a8a45a94b4bf6c45acb2153e60
APlatonenkov <platonenkov87@gmail.com>
2021-09-30 14:40:11 +0300

    документация

--- MathCore.EF7.Interfaces/Entities/IEntity.cs
+++ MathCore.EF7.Interfaces/Entities/IEntity.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
-
-namespace MathCore.EF7.Interfaces.Entities
+﻿namespace MathCore.EF7.Interfaces.Entities
 {
     /// <summary>Сущность</summary>
     /// <typeparam name="TKey">Тип первичного ключа</typeparam>
